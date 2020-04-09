839bab39f43b4b24cf209b27e17fca26bd1bde93
masterUNG <phrombutr@gmail.com>
2020-04-09 10:06:51 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'package:ungsenete/screens/my_service.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.purple),
       home: MyService(),
     );
